8acde53b1415bf9be7364410869f2a014d97f4e3
creedenceclearwater <creedenceclearwater@LAPTOP-BIT7KUH6>
2022-09-21 11:22:54 +0300

    sessli harfler

---
+++ ikiniMiniProje/src/ikiniMiniProje/Main.java
@@ -0,0 +1,22 @@
+package ikiniMiniProje;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		char harf = 'K';
+
+	    switch (harf){
+	        case 'A':
+	        case 'U':
+	        case 'I':
+	        case 'O':
+	        case 'E':
+	            System.out.println("Sesli Kalin Harf");
+	            break;
+	        default:
+	            System.out.println("Sessiz ince Harf");
+	    }
+	    }
+	}
+
+
